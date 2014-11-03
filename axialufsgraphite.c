cc7b92abdd5044a1b8d61d404f9b77f053caa8af
kylophone <kylophone@gmail.com>
2014-11-02 22:43:47 -0600

    Outputs loudness as graphite plaintext protocol

--- axialufsgraphite.c
+++ axialufsgraphite.c
@@ -19,36 +19,46 @@ int main(int argc, const char *argv[])
   double audioPayload[480]; // 480 audio samples per packet (max.) 
   ebur128_state* state = NULL;
   double shortTermLoudness;
-  uint32_t frameCounter = 0;
+  uint16_t frameCounter = 0;
   const char *multicastAddr = argv[1];
+
+  int graphiteSocket, n;
+  struct sockaddr_in servaddr;
+
+  /* Setup Graphite Socket */
+  graphiteSocket = socket(AF_INET,SOCK_STREAM,0);
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(2003);
+  inet_pton(AF_INET, "127.0.0.1", &(servaddr.sin_addr));
+  connect(graphiteSocket,(struct sockaddr *) &servaddr, sizeof(servaddr));
  
-  /* set up socket */
+  /* set up multicast listener socket */
   sock = socket(AF_INET, SOCK_DGRAM, 0);
   int reuse = 1;
   if (setsockopt(sock, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) == -1) {
       fprintf(stderr, "setsockopt: %d\n", errno);
       return 1;
   }
-  
   memset(&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
-  addr.sin_port = htons(5004); // Livewire is always on port 5004.
+  addr.sin_port = htons(5004);
   addr.sin_addr.s_addr = inet_addr(multicastAddr);
   addrlen = sizeof(addr);
-
   if (bind(sock, (struct sockaddr*) &addr, sizeof(addr)) == -1) {
       fprintf(stderr, "bind: %d\n", errno);
       return 1;
   }
-
   mreq.imr_multiaddr.s_addr = inet_addr(multicastAddr);         
   mreq.imr_interface.s_addr = htonl(INADDR_ANY);
   setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&mreq, sizeof(mreq));
 
+  /* init ebur128 state*/
   state = malloc((size_t) sizeof(ebur128_state*));
   state = ebur128_init((unsigned) 2, (unsigned) 48000, EBUR128_MODE_S);
 
   /* main loop */
+  char grapiteOutputBuffer[100];
   while(1){
     packetLength = recvfrom(sock, packet, sizeof(packet), 0, (struct sockaddr *) &addr, (socklen_t *) &addrlen);
     
@@ -65,11 +75,12 @@ int main(int argc, const char *argv[])
 
     ebur128_add_frames_double(state, audioPayload, (size_t) ((packetLength - 12) / 6));
     frameCounter += ((packetLength - 12) / 6);
-    if (frameCounter >= 48000) {
+    if (frameCounter >= 4800) {
       frameCounter = 0;
       ebur128_loudness_shortterm(state, &shortTermLoudness);
       //ebur128_loudness_momentary(state, &momentaryLoudness);
-      fprintf(stderr, "%-2.1f\n", shortTermLoudness);
+      sprintf(grapiteOutputBuffer, "kylophone %2.1f %d\n", shortTermLoudness, (int) time(NULL)); //substitute your metric name for `kylophone.` 
+      send(graphiteSocket, grapiteOutputBuffer, (strlen(grapiteOutputBuffer)), 0);
     } 
   }
   free(state);
